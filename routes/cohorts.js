6e5da069ceb72a640654d607f324c8134bd555e7
WebMats <lorlando127@gmail.com>
2019-02-20 17:21:22 -0500

    added POST and GET routes for students and cohorts endpoints

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -2,10 +2,36 @@ const express = require('express');
 const db = require('../data/dbConfig');
 const router = express.Router();
 
-
+router.post('', async (req, res, next) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(404).json({errorMessage: "Please provide us with a name for the new cohort."})
+    }
+    try {
+        const [id] = await db('cohorts').insert({name});
+        res.status(201).json({id});
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({errorMessage: `Could not create cohort ${name}`})
+    }
+})
 router.get('', async (req, res, next) => {
-    const cohorts = await db('cohorts');
-    res.status(200).json(cohorts);
+    try {
+        const cohorts = await db('cohorts');
+        res.status(200).json(cohorts);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({errorMessage: "Could not fetch cohorts."})
+    }
+})
+router.get('/:id', async (req, res, next) => {
+    try {
+        const cohort = await db('cohorts').where({id: req.params.id}).first()
+        res.status(200).json(cohort);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({errorMessage: "Could not fetch cohort."})
+    }
 })
 
 module.exports = router
--- routes/students.js
+++ routes/students.js
@@ -4,9 +4,36 @@ const router = express.Router();
 
 
 
+router.post('', async (req, res, next) => {
+    const { name, cohort_id } = req.body;
+    if (!name || !cohort_id) {
+        return res.status(404).json({errorMessage: "Please provide both name and cohort of new student."})
+    }
+    try {
+        const [id] = await db('students').insert({name, cohort_id});
+        res.status(201).json({id});
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({errorMessage: `Could not add ${name} to cohort with id: ${cohort_id}`})
+    }
+}) 
 router.get('', async (req, res, next) => {
-    const students = await db('students');
-    res.status(200).json(students);
+    try {
+        const students = await db('students');
+        res.status(200).json(students);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({errorMessage: "Could not fetch students."})
+    }
+})
+router.get('/:id', async (req, res, next) => {
+    try {
+        const student = await db('students').where({id: req.params.id});
+        res.status(200).json(student)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({errorMessage: "Could not fetch student."})
+    }
 })
 
 
